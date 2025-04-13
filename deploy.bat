fe5d17a36f88ed9b4292f56cd4c95ca08f7e9943
Chris Gerth <chrisgerth010592@gmail.com>
2025-04-13 18:32:29 -0500

    Better deploy, catches errors and is quieter

--- deploy.bat
+++ deploy.bat
@@ -1,17 +1,39 @@
-mpremote mkdir :private
+@echo off
+mpremote mkdir :_private
 mpremote mkdir :www
+mpremote mkdir :wpilib
 
 :: copy all source files
 for %%f in (*.py) do (
     mpremote fs cp %%f :%%f
+    if errorlevel 1 (
+        echo Failed to copy %%f
+        exit /b 1
+    )
 )
 
-for %%f in (private/*.py) do (
-    mpremote fs cp private/%%f :private/%%f
+for %%f in (_private/*.py) do (
+    mpremote fs cp _private/%%f :_private/%%f
+    if errorlevel 1 (
+        echo Failed to copy %%f
+        exit /b 1
+    )
+)
+
+for %%f in (wpilib/*.py) do (
+    mpremote fs cp wpilib/%%f :wpilib/%%f
+    if errorlevel 1 (
+        echo Failed to copy %%f
+        exit /b 1
+    )
 )
 
 for %%f in (www/*.*) do (
     mpremote fs cp www/%%f :www/%%f
+    if errorlevel 1 (
+        echo Failed to copy %%f
+        exit /b 1
+    )
 )
 
 mpremote soft-reset
