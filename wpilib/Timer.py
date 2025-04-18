d2fbd9810cba2377a5b32799c3697e28c7881b58
Chris Gerth <chrisgerth010592@gmail.com>
2025-04-17 21:34:49 -0500

    moar wpilib

---
+++ wpilib/Timer.py
@@ -0,0 +1,11 @@
+import time
+
+class Timer():
+
+    @staticmethod
+    def getFPGATimestamp():
+        """
+        Returns the current time in seconds since the FPGA started running.
+        This is a floating-point number that represents the time in seconds.
+        """
+        return time.ticks_us()/ 1000000.0
--- wpilib/__init__.py
+++ wpilib/__init__.py
@@ -6,5 +6,17 @@ from .Keyboard import Keyboard
 from .SlewRateLimiter import SlewRateLimiter
 from .MedianFilter import MedianFilter
 from .Debouncer import Debouncer, DebounceType
+from .Timer import Timer
 
-__all__ = ['Servo', 'Motor', 'Gyro', 'Encoder', 'Keyboard', 'SlewRateLimiter', 'MedianFilter', 'Debouncer', 'DebounceType']
+__all__ = [
+    'Servo', 
+    'Motor', 
+    'Gyro', 
+    'Encoder', 
+    'Keyboard', 
+    'SlewRateLimiter', 
+    'MedianFilter', 
+    'Debouncer', 
+    'DebounceType',
+    'Timer',
+]
